a2bf7e91bf5a2639e5f60eda42323ba52dce647f
Alexandr <jwchaganav@gmail.com>
2022-11-18 17:05:29 +0400

    пока без среднего

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ int rows = rnd.Next(2,10);
 int columns = rnd.Next(2,10);
 int[,] numbers = new int[rows,columns];
 int[] sumTotalColumn = new int[columns];
-int[] averColumn = new int[columns];
+double[] averColumn = new double[columns];
 
 FillArrayNull(sumTotalColumn); // заполним итоги нулями
 FillRandomArray(numbers); // заполним массив
@@ -15,13 +15,13 @@ AverageColumns(numbers, sumTotalColumn, averColumn);
 Console.WriteLine("==== Итоги ====");
 PrintArray(sumTotalColumn);
 Console.WriteLine("=== среднее ===");
-PrintArray(averColumn);
+PrintDoubleArray(averColumn);
 
 
 
 
 // Функции
-void AverageColumns(int[,] array, int[] sumCol, int[] averCol)
+void AverageColumns(int[,] array, int[] sumCol, double[] averCol)
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
@@ -29,7 +29,7 @@ void AverageColumns(int[,] array, int[] sumCol, int[] averCol)
         {
             sumCol[j]+= array[i,j];
         }
-        averCol[i] = sumCol[i]/array.GetLength(0);
+        //averCol[i] = sumCol[i]/array.GetLength(1);
     }
 }
 
@@ -63,6 +63,14 @@ void Print2DArray(int[,] array)
     }
 }
 void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void PrintDoubleArray(double[] array)
 {
     for(int i = 0; i < array.Length; i++)
     {
